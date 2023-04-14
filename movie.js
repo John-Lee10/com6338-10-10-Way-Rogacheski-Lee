0eb75b5826faea77317ec5f38b469ad13f60e2c8
nathalia-uf <112099626+nathalia-uf@users.noreply.github.com>
2023-04-14 13:37:29 -0400

    nathalia 6

--- movie.js
+++ movie.js
@@ -7,7 +7,7 @@ form.onsubmit = function(e) {
   var searchTerm = this.movie-title.value
   if (!searchTerm) return
   form.movie-title.value = ""
-  fetch(" http://www.omdbapi.com/?t=" + searchTerm + "&type=series&apikey=4975ce6")
+  fetch(" http://www.omdbapi.com/?t=" + searchTerm + "&type=series&apikey=4975ce62")
   .then(function(res) {
       if(res.status !== 200){
           throw new Error('Show not found')
@@ -20,24 +20,31 @@ form.onsubmit = function(e) {
     var h2 = document.createElement('h2')
     h2.textContent = data.Title
     MovieDiv.appendChild(h2)
+    
     var Year = document.createElement('p')
-    Year.textContent = "Aired: " + data.Year
+    Year.textContent = "Released: " + data.Year
     MovieDiv.appendChild(Year)
+
     var Rating = document.createElement('p')
-    Rating.textContent = "Aired: " + data.Rating
+    Rating.textContent = "Rating: " + data.Rating
     MovieDiv.appendChild(Rating)
+
     var Genre = document.createElement('p')
     Genre.textContent = "Genre: " + data.Genre
     MovieDiv.appendChild(Genre)
+
     var Director = document.createElement('p')
-    Director.textContent = "Aired: " + data.Director
+    Director.textContent = "Director: " + data.Director
     MovieDiv.appendChild(Director)
+
     var Actors = document.createElement('p')
-    Actors.textContent = "Cast: " + data.Actors
+    Actors.textContent = "Actors: " + data.Actors
     MovieDiv.appendChild(Actors)
+
     var Plot = document.createElement('p')
-    Plot.textContent = "Number of Seasons: " + data.Plot
+    Plot.textContent = "Plot: " + data.Plot
     MovieDiv.appendChild(Plot)
+
     var poster = document.createElement('img')
     poster.src = data.Poster
     poster.alt = data.Title
