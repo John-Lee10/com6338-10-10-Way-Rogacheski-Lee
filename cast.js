e769e17f24460f777f71c63583649c378bdd52e1
John Lee <john.lee@ufl.edu>
2023-04-15 12:34:08 -0400

    cast page update

--- cast.js
+++ cast.js
@@ -7,10 +7,10 @@ form.onsubmit = function(e) {
     var searchTerm = this.actorName.value
     if (!searchTerm) return
     form.actorName.value = ""
-    fetch("https://api.themoviedb.org/3/movie/550?api_key=5b96fe3be7fae014b08a2548f8f96219" + searchTerm)
+    fetch("https://api.themoviedb.org/3/search/person?api_key=5b96fe3be7fae014b08a2548f8f96219&query=" + searchTerm)
     .then(function(res) {
         if(res.status !== 200){
-            throw new Error('Show not found')
+            throw new Error('Actor not found')
         }
         return res.json()
     })
@@ -19,20 +19,17 @@ form.onsubmit = function(e) {
         actorDiv.innerHTML = ""
 
         var h2 = document.createElement('h2')
-        h2.textContent = data.name
+        h2.textContent = data.results[0].name
         actorDiv.appendChild(h2)
 
-        var birthday = document.createElement('p')
-        birthday.textContent = "Birthday: " + data.birthday
-        actorDiv.appendChild(birthday)
-
-        var place_of_birth = document.createElement('p')
-        cast.textContent = "Place of Birth: " + data.place_of_birth
-        actorDiv.appendChild(place_of_birth)
+        var popularity = document.createElement('p')
+        popularity.textContent = "Popularity Rating: " + data.results[0].popularity
+        actorDiv.appendChild(popularity)
 
         var biography = document.createElement('p')
-        biography.textContent = "Biography: " + data.biography
+        biography.textContent = "Known For: " + data.results[0].known_for[0].title + ", " + data.results[0].known_for[1].title + ", and " + data.results[0].known_for[2].title
         actorDiv.appendChild(biography)
+
     })
     .catch(function(err){
         actorDiv.innerHTML = err.message
